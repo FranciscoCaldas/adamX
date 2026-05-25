502190c634706b0ee2701229a4c70e90e9ed99e5
FranciscoCaldas <f.caldas@campus.fct.unl.pt>
2026-05-25 20:37:14 +0100

    slurm

---
+++ run_slurm_hyper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#SBATCH -p phd
+#SBATCH --job-name=cexp_nc
+#SBATCH --output=output/out_hyper_%j.txt
+#SBATCH --gpus=2g.20g:1
+#SBATCH -c 4
+
+# SET MINICONDA_PATH HERE
+MINICONDA_PATH=/home/f.caldas/miniconda3/ #EX:/home/<USER>/miniconda3/ (without any leading or trailing spaces)
+
+
+dataset=${DATASET:-mnist}
+optimizer=${optimizer:-adam}
+NUM_RUNS=${NUM_RUNS:-10}
+export KMP_DUPLICATE_LIB_OK=TRUE
+if [ -z "$MINICONDA_PATH" ]
+then
+	self=$(basename "$0")
+	echo "JOB SUBMISSION FAILED. PLEASE SET MINICONDA_PATH on $self"
+else
+	source "$MINICONDA_PATH"etc/profile.d/conda.sh
+	conda activate /data/f.caldas/miniconda3/envs/adamx
+	echo "TASK: $TASK MODEL: $MODEL DATASET: $DATASET OPTIMIZER: $optimizer NUM_RUNS: $NUM_RUNS"
+	srun python scripts/hyper_search.py --dataset $dataset --optimizer $optimizer 
+fi
